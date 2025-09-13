bb7e427a3bcab6884646bd3ce0af013dc67b107a
Md Imran Khan <imrananwar307@gmail.com>
2025-09-13 15:56:35 +0600

    Day 01: Two Sum solution

---
+++ Day01/soution.js
@@ -0,0 +1,12 @@
+var createCounter = function(n) {
+    let count = n-1;
+    return function() {
+        count +=1;
+        return count;
+    };
+};
+
+const counter = createCounter(10)
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
